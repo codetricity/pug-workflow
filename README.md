a5f0fe3dcfe18d4a143d745155461c91ece367be
Craig <craig@oppkey.com>
2023-04-01 09:56:00 -0700

    minimal app

---
+++ README.md
@@ -0,0 +1,24 @@
+# gulp workflow test
+
+This is a workflow example using pug to prepare for the use
+of the sparrow bootstrap template system. Instead of editing
+the HTML files directly, the sparrow system
+uses pug, sass, and gulp. This makes it easier to handle components
+such as the menu system.
+
+* gulp
+* node v18.15.0
+* pug
+* sass with node-sass (sass module does not work)
+* browsersync
+
+requires src/scss/style.scss file
+
+```text
+npm i -g gulp-cli
+npm i -D gulp
+npm i -D gulp-pug
+npm i -D gulp-sass
+npm i -D browser-sync
+npm i -D node-sass
+```
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const pug = require('gulp-pug');
+const browsersync = require('browser-sync').create();
+
+function scssTask(){
+    return src('src/scss/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(dest('dist', { sourcemaps: '.'}));
+    }
+
+
+function pugTask() {
+        return src("src/pug/*.pug")
+        .pipe(pug({pretty: true}))
+        .pipe(dest("dist"));
+    }
+
+
+function browsersyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+    cb();
+}
+
+
+function browsersyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+function watchTask() {
+    watch("src/pug/*.pug", series(pugTask, browsersyncReload));
+    watch("dist/*.html", browsersyncReload)
+    watch(['src/scss/*.scss'], series(scssTask, browsersyncReload));
+}
+
+
+
+exports.default = series(
+    scssTask,
+    pugTask,
+    browsersyncServe,
+    watchTask
+);
